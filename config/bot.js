60006db8e7508640aa3fc949300683e2e85207a5
ZéLotDev <90394485+ZeLotDev@users.noreply.github.com>
2021-09-19 12:12:50 +0700

    config for changing token and prefix

---
+++ config/bot.js
@@ -0,0 +1,21 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':notes:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'YOUR_TOKEN_HERE',
+        prefix: 'YOUR_PREFIX_HERE',
+        activity: {
+            name : "ACTIVITY_NAME", //example "coding music bot"
+            type : 'ACTIVITY_TYPE' //LISTENING, STREAMING, PLAYING, WATCHING
+        },
+        status: 'STREAMING'
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
